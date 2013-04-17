27faed412a58191cedcef3d9f3c0343ae2c91155
Rogerio <rogerio16jr@hotmail.com>
2013-04-16 22:02:58 -0300

    16042013 - Teste alter register

---
+++ CleanRegister/CleanRegister/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace CleanRegister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btSearch = new System.Windows.Forms.Button();
+            this.txtAdress = new System.Windows.Forms.TextBox();
+            this.txtKeyWord = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstListKeys = new System.Windows.Forms.ListBox();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btSearch
+            // 
+            this.btSearch.Location = new System.Drawing.Point(295, 70);
+            this.btSearch.Name = "btSearch";
+            this.btSearch.Size = new System.Drawing.Size(75, 23);
+            this.btSearch.TabIndex = 0;
+            this.btSearch.Text = "Search";
+            this.btSearch.UseVisualStyleBackColor = true;
+            this.btSearch.Click += new System.EventHandler(this.btSearch_Click);
+            // 
+            // txtAdress
+            // 
+            this.txtAdress.Location = new System.Drawing.Point(91, 21);
+            this.txtAdress.Name = "txtAdress";
+            this.txtAdress.Size = new System.Drawing.Size(279, 20);
+            this.txtAdress.TabIndex = 1;
+            // 
+            // txtKeyWord
+            // 
+            this.txtKeyWord.Location = new System.Drawing.Point(92, 47);
+            this.txtKeyWord.Name = "txtKeyWord";
+            this.txtKeyWord.Size = new System.Drawing.Size(278, 20);
+            this.txtKeyWord.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Caminho:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Palavra Chave:";
+            // 
+            // lstListKeys
+            // 
+            this.lstListKeys.FormattingEnabled = true;
+            this.lstListKeys.Location = new System.Drawing.Point(15, 99);
+            this.lstListKeys.Name = "lstListKeys";
+            this.lstListKeys.Size = new System.Drawing.Size(355, 186);
+            this.lstListKeys.TabIndex = 5;
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(143, 291);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(99, 23);
+            this.btDelete.TabIndex = 6;
+            this.btDelete.Text = "Apagar Tudo";
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(327, 291);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(43, 13);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "Total: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 355);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.lstListKeys);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKeyWord);
+            this.Controls.Add(this.txtAdress);
+            this.Controls.Add(this.btSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btSearch;
+        private System.Windows.Forms.TextBox txtAdress;
+        private System.Windows.Forms.TextBox txtKeyWord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstListKeys;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ CleanRegister/CleanRegister/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace CleanRegister
+{
+    public partial class Form1 : Form
+    {
+        List<string> sListFolderToDelete = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtAdress.Text = "SOFTWARE\\WOW6432Node\\Microsoft\\Classes\\CLSID";
+            txtKeyWord.Text = "C:\\Program Files\\Processware\\O2P";
+                 
+        }
+
+        private string[] GetFolder(string sAddress)
+        {
+            RegistryKey root = Registry.LocalMachine.OpenSubKey(sAddress, true);
+            return root.GetSubKeyNames();
+        }
+
+        private void btSearch_Click(object sender, EventArgs e)
+        {
+            bool bFound = false;
+            string sAddressN0 = txtAdress.Text;
+            
+            string[] sFolderCLSID = GetFolder(sAddressN0);
+
+            foreach (string sFolder in sFolderCLSID)
+            {
+                string sAddressN1 = sAddressN0 + "\\" + sFolder;
+                string[] sNivel1 = GetFolder(sAddressN1);
+                
+                //001
+                foreach (string sFolderN1 in sNivel1)
+                {
+                    string sAddressN2 = sAddressN1 + "\\" + sFolderN1;
+
+                    RegistryKey N2 = Registry.LocalMachine.OpenSubKey(sAddressN2, true);
+                    string[] sF1 = N2.GetValueNames();
+
+                    foreach (string sFile1 in sF1)
+                    {
+                        if (N2.GetValue(sFile1).ToString().Replace("\\", "") == txtKeyWord.Text.Replace("\\", ""))
+                        {
+                            bFound = true;
+                            break;
+                        }
+                    }
+
+                    if (bFound)
+                    {
+                        sListFolderToDelete.Add(sAddressN1);
+                       bFound = false;
+                    }
+                }
+            }
+
+            lstListKeys.DataSource = sListFolderToDelete;
+            lstListKeys.Update();
+            lblTotal.Text = "Total: " + sListFolderToDelete.Count;
+
+        }
+
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            foreach (string sItem in sListFolderToDelete)
+            {
+                Registry.LocalMachine.DeleteSubKeyTree(sItem);
+            }
+
+            sListFolderToDelete.Clear();
+            lstListKeys.DataSource = sListFolderToDelete;
+            lstListKeys.Update();
+            lblTotal.Text = "Total: " + sListFolderToDelete.Count;
+        }
+    }
+}
